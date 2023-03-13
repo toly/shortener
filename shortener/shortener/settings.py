31227e641ba97a38aeac9811ee4316244f2ed55c
toly <tolytrader@gmail.com>
2023-03-13 19:18:44 +0300

    add host

--- shortener/shortener/settings.py
+++ shortener/shortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6dc^+5%4@16)!*t0-9%7x6*m9@e*wigycyv(ky#mc-@%#*+)kv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wildberiec.ru']
 
 
 # Application definition
