1e1057a6758d26ce2640d9b5e8e6eae59f4032a8
toly <tolytrader@gmail.com>
2023-03-13 19:33:35 +0300

    fix

--- shortener/shortener/settings.py
+++ shortener/shortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6dc^+5%4@16)!*t0-9%7x6*m9@e*wigycyv(ky#mc-@%#*+)kv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wildberiec.ru']
+ALLOWED_HOSTS = ['wildberiec.ru', '127.0.0.1']
 
 
 # Application definition
